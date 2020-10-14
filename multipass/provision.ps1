a19532da6248604cf45bbdf4089acdc2fccfdeb6
Kostiantyn Vorobiov <konstantin.vorobyev@bimodalconsulting.com>
2020-10-14 18:14:14 +0300

    host output (#2)

--- multipass/provision.ps1
+++ multipass/provision.ps1
@@ -8,3 +8,12 @@ Param(
 For ($i=0; $i -lt $count; $i++) {
     multipass launch -m $memory -d $disk -c $cpu --cloud-init $cloud_init
 }
+
+$list_machines = multipass list --format json | ConvertFrom-Json | select -expand list | select name, ipv4 
+
+"[all]"|Out-File -append hosts
+ForEach($machine in $list_machines){
+    $name = $machine.name
+    $ipv4 = $machine.ipv4
+    "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $name,$ipv4[0] | Out-File -append hosts 
+}
