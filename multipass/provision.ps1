9b9328484f671ad7b9546272ffd4430cddbbf541
vorkos <boss138@gmail.com>
2020-10-15 13:48:57 +0300

    user and keys setup

--- multipass/provision.ps1
+++ multipass/provision.ps1
@@ -1,5 +1,5 @@
 Param(
-  [int] $count = 2,
+  [int] $count = 4,
   [int] $cpu = 2,
   [string] $memory = "4G",
   [string] $disk = "20G",
@@ -11,9 +11,19 @@ For ($i=0; $i -lt $count; $i++) {
 
 $list_machines = multipass list --format json | ConvertFrom-Json | select -expand list | select name, ipv4 
 
-"[all]"|Out-File -append hosts
-ForEach($machine in $list_machines){
-    $name = $machine.name
-    $ipv4 = $machine.ipv4
-    "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $name,$ipv4[0] | Out-File -append hosts 
+"[hadoop]"|Out-File -append hosts
+For($i=0; $i -lt $list_machines.Length; $i++) {
+    $name = $list_machines[$i].name
+    $ipv4 = $list_machines[$i].ipv4
+    if($i -eq 0 ) {
+      $role = "master"
+      $master = $list_machines[$i]
+      $master.ipv4 = $list_machines[$i].ipv4[0]
+    } else {
+      $role = "worker"
+    }
+    "{0,-20} ansible_user=ansible  ansible_host={1,-20} role={2,-20}" -f $name,$ipv4[0],$role | Out-File -append hosts 
 }
+
+"[hadoop:vars]"|Out-File -append hosts
+"master_hostname={0,-20} `nmaster_ip={1,-20}" -f $master.name,$master.ipv4 | Out-File -append hosts
