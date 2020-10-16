115b972c494b7ffb3ab5688e22e870a303dc7899
vorkos <boss138@gmail.com>
2020-10-16 12:16:47 +0300

    add workers and master group

--- multipass/provision.ps1
+++ multipass/provision.ps1
@@ -22,8 +22,21 @@ For($i=0; $i -lt $list_machines.Length; $i++) {
     } else {
       $role = "worker"
     }
+    $list_machines[$i] | Add-Member -NotePropertyName role -NotePropertyValue $role
     "{0,-20} ansible_user=ansible  ansible_host={1,-20} role={2,-20}" -f $name,$ipv4[0],$role | Out-File -append hosts 
 }
 
 "[hadoop:vars]"|Out-File -append hosts
 "master_hostname={0,-20} `nmaster_ip={1,-20}" -f $master.name,$master.ipv4 | Out-File -append hosts
+"[workers]"|Out-File -append hosts
+foreach ($worker in $list_machines) {
+    if ($worker.role -eq "worker") {
+        "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $worker.name,$worker.ipv4[0] | Out-File -append hosts 
+    }
+}
+"[masters]"|Out-File -append hosts
+foreach ($master in $list_machines) {
+    if ($master.role -eq "master") {
+        "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $worker.name,$master.ipv4[0] | Out-File -append hosts 
+    }
+}
