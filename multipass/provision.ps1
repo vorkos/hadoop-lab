21b12cda4b608abbad42dacfe02a6df780b82a35
Kostiantyn Vorobiov <konstantin.vorobyev@bimodalconsulting.com>
2020-10-16 14:08:03 +0300

    Feature/hadoop install (#3)
    * user and keys setup
    
    * download and untar hadoop
    
    * add workers and master group
    
    * hadoop installed
    
    * format code

--- multipass/provision.ps1
+++ multipass/provision.ps1
@@ -1,5 +1,5 @@
 Param(
-  [int] $count = 2,
+  [int] $count = 4,
   [int] $cpu = 2,
   [string] $memory = "4G",
   [string] $disk = "20G",
@@ -9,11 +9,30 @@ For ($i=0; $i -lt $count; $i++) {
     multipass launch -m $memory -d $disk -c $cpu --cloud-init $cloud_init
 }
 
-$list_machines = multipass list --format json | ConvertFrom-Json | select -expand list | select name, ipv4 
+$list_machines = multipass list --format json | ConvertFrom-Json | Select -expand list | Select name, ipv4 
 
-"[all]"|Out-File -append hosts
-ForEach($machine in $list_machines){
-    $name = $machine.name
-    $ipv4 = $machine.ipv4
-    "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $name,$ipv4[0] | Out-File -append hosts 
+"[hadoop]"|Out-File -append hosts
+For($i=0; $i -lt $list_machines.Length; $i++) {
+    if($i -eq 0 ) {
+      $role = "master"
+    } else {
+      $role = "worker"
+    }
+    $list_machines[$i] | Add-Member -NotePropertyName role -NotePropertyValue $role
+    "{0,-20} ansible_user=ansible  ansible_host={1,-20} role={2,-20}" -f $list_machines[$i].name,$list_machines[$i].ipv4[0],$role | Out-File -append hosts 
+}
+
+"[hadoop:vars]"|Out-File -append hosts
+foreach($master in $list_machines | Where role -eq "master" ){
+    "master_hostname={0,-20} `nmaster_ip={1,-20}" -f $master.name,$master.ipv4[0] | Out-File -append hosts
+}
+
+"[masters]"|Out-File -append hosts
+foreach ($master in $list_machines | Where role -eq "master" ) {
+    "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $master.name,$master.ipv4[0] | Out-File -append hosts 
+}
+
+"[workers]"|Out-File -append hosts
+foreach ($worker in $list_machines | Where role -eq "worker") {
+        "{0,-20} ansible_user=ansible  ansible_host={1,-20}" -f $worker.name,$worker.ipv4[0] | Out-File -append hosts 
 }
